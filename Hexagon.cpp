80a3f2e8152b15ad8e053b6266802d0707446465
ZK <liuzq7@gmail.com>
2014-05-28 09:32:26 +0800

    update demo jpg

--- Hexagon.cpp
+++ Hexagon.cpp
@@ -45,11 +45,6 @@ Hexagon& Hexagon::Move (eDirection dir, int times)
 }
 void CGAffineToGL (const CCAffineTransform *t, GLfloat *m)
 {
-    // | m[0] m[4] m[8]  m[12] |     | m11 m21 m31 m41 |     | a c 0 tx |
-    // | m[1] m[5] m[9]  m[13] |     | m12 m22 m32 m42 |     | b d 0 ty |
-    // | m[2] m[6] m[10] m[14] | <=> | m13 m23 m33 m43 | <=> | 0 0 1  0 |
-    // | m[3] m[7] m[11] m[15] |     | m14 m24 m34 m44 |     | 0 0 0  1 |
-
     m[2] = m[3] = m[6] = m[7] = m[8] = m[9] = m[11] = m[14] = 0.0f;
     m[10] = m[15] = 1.0f;
     m[0] = t->a;
